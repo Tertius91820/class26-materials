c009858f0283abf034ec59982600a0ce1e494587
tertius Jubber <tjubber@gmail.com>
2023-05-29 13:40:11 +0200

    playing with API`s

--- objects-constructor-fun/js/main.js
+++ objects-constructor-fun/js/main.js
@@ -1,1 +1,18 @@
 //Create a constructor with 4 properties and 3 methods
+function MakePizza(pizzaSize,pizzaBase,pizzaTemperature,pizzaCheese){
+  this.size = pizzaSize
+  this.base = pizzaBase
+  this.temperature = pizzaTemperature
+  this.cheese = pizzaCheese
+  this.delivery = function(){
+    console.log(`deliver pizza ${pizza.temperature}`)
+  }
+  this.smell = function(){
+    console.log(`this smells awesome!`)
+  }
+  this.order = function(){
+    console.log( "received")
+  }
+}
+
+let massivePizza = new MakePizza("massive","stacked","pipin hot","extra")
