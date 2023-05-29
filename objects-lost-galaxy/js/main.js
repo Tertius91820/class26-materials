47d805811aac17d2c98f5291f6e84b74650332f2
Tertius Jubber <tjubber@gmail.com>
2023-05-29 11:31:25 +0200

    added constructor

--- objects-lost-galaxy/js/main.js
+++ objects-lost-galaxy/js/main.js
@@ -17,3 +17,20 @@ pizza.order = function(){
   console.log( "received")
 }
 
+function MakePizza(pizzaSize,pizzaBase,pizzaTemperature,pizzaCheese){
+  this.size = pizzaSize
+  this.base = pizzaBase
+  this.temperature = pizzaTemperature
+  this.cheese = pizzaCheese
+  this.delivery = function(){
+    console.log(`deliver pizza ${pizza.temperature}`)
+  }
+  this.smell = function(){
+    console.log(`this smells awesome!`)
+  }
+  this.order = function(){
+    console.log( "received")
+  }
+}
+
+let massivePizza = new MakePizza("massive","stacked","pipin hot","extra")
